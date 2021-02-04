012c7e8a3f5b9a5a60bcfc7cfac826f5475ee767
julia-bindi <juliabindi@gmail.com>
2021-02-04 19:39:27 -0300

    codigo

--- arvore_b.py
+++ arvore_b.py
@@ -1,6 +1,10 @@
 class ArvoreB:
+    def __init__(self):
+        self.raiz:'Pagina'
+        self.m:int
+        self.mm:int
     class Pagina:
         def __init__(self,mm):
                 self.n = 0
-                self.r()
-                self.p()
+                self.r(mm)
+                self.p = Pagina(mm+1)
